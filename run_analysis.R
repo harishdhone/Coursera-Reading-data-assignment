e2eae11109fc58ad36e975c8d731b40a5a033720
harishdhone <36388782+harishdhone@users.noreply.github.com>
2018-05-06 20:15:15 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -7,7 +7,7 @@ library(tidyr)
 
 if(!file.exists("data")){dir.create("data")}
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
-              "./data/data.zip",method = "curl")
+              "./data/data.zip",method = "curl") #Method = curl only for Mac/Unix
 
 #extracting data from zip file
 unzip("./data/data.zip",exdir = "./data")
